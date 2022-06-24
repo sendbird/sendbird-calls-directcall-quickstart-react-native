312f8c16ea62b1c288d9d8001ca3252bbb76195e
bang9 <gusrn1423@naver.com>
2022-06-25 02:38:47 +0900

    setup ios voip

--- ios/callapp/AppDelegate.h
+++ ios/callapp/AppDelegate.h
@@ -1,7 +1,8 @@
 #import <React/RCTBridgeDelegate.h>
 #import <UIKit/UIKit.h>
+#import <PushKit/PushKit.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
+@interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate, PKPushRegistryDelegate>
 
 @property (nonatomic, strong) UIWindow *window;
 
--- ios/callapp/AppDelegate.mm
+++ ios/callapp/AppDelegate.mm
@@ -6,6 +6,14 @@
 
 #import <React/RCTAppSetupUtils.h>
 
+#import <CoreMedia/CoreMedia.h>
+#import <WebRTC/WebRTC.h>
+#import <PushKit/PushKit.h>
+#import <AVKit/AVKit.h>
+#import <SendBirdCalls/SendBirdCalls-Swift.h>
+#import <RNVoipPushNotificationManager.h>
+#import <RNCallKeep.h>
+
 #if RCT_NEW_ARCH_ENABLED
 #import <React/CoreModulesPlugins.h>
 #import <React/RCTCxxBridgeDelegate.h>
@@ -80,6 +88,70 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
 #endif
 
   return initProps;
+}
+
+// MARK: - VoIP Notification - Receive token
+- (void)pushRegistry:(PKPushRegistry *)registry didUpdatePushCredentials:(PKPushCredentials *)pushCredentials forType:(PKPushType)type
+{
+  [RNVoipPushNotificationManager didUpdatePushCredentials:pushCredentials forType:(NSString *)type];
+}
+
+// MARK: - VoIP Notification - Receive incoming call
+/**
+ * This being called after voip registration
+ * so you can register voip on the JS side, after set `SendbirdCalls.onRinging` and `RNCallKeep.addListener`
+ *
+ * 0. voip notification wake your app
+ * 1. [Native] App is start
+ * 2. [JS] JS bridge created and your React-Native app is mounted
+ * 3. [JS] call SendbirdCalls.initialize()
+ * 4. [JS] set SendbirdCalls.onRinging
+ * 5. [JS] set RNCallKeep.addListener
+ * 6. [JS] RNVoipPushNotification.registerVoipToken() >> it means register voip
+ * 7. [Native] didReceiveIncomingPushWithPayload called
+ * 8-1. [Native] SendbirdCalls.didReceiveIncomingPush >> it will trigger Ringing event
+ * 8-2. [Native] report to CallKit
+ * 9. [JS] onRinging listener called
+ */
+- (void)pushRegistry:(PKPushRegistry *)registry didReceiveIncomingPushWithPayload:(PKPushPayload *)payload forType:(PKPushType)type withCompletionHandler:(void (^)())completion
+{
+  // WARN: If you don't report to CallKit, the app will be shut down.
+  [SBCSendBirdCall pushRegistry:registry didReceiveIncomingPushWith:payload for:type completionHandler:^(NSUUID * _Nullable uuid) {
+    if(uuid != nil) {
+      // Report valid call
+      SBCDirectCall* call = [SBCSendBirdCall callForUUID: uuid];
+      [RNCallKeep reportNewIncomingCall: [uuid UUIDString]
+                                 handle: [[call remoteUser] userId]
+                             handleType: @"generic"
+                               hasVideo: [call isVideoCall]
+                    localizedCallerName: [[call remoteUser] nickname]
+                        supportsHolding: YES
+                           supportsDTMF: YES
+                       supportsGrouping: YES
+                     supportsUngrouping: YES
+                            fromPushKit: YES
+                                payload: [payload dictionaryPayload]
+                  withCompletionHandler: completion];
+    } else {
+      // Report and end invalid call
+      NSUUID* uuid = [NSUUID alloc];
+      NSString* uuidString = [uuid UUIDString];
+      
+      [RNCallKeep reportNewIncomingCall: uuidString
+                                 handle: @"invalid"
+                             handleType: @"generic"
+                               hasVideo: NO
+                    localizedCallerName: @"invalid"
+                        supportsHolding: NO
+                           supportsDTMF: NO
+                       supportsGrouping: NO
+                     supportsUngrouping: NO
+                            fromPushKit: YES
+                                payload: [payload dictionaryPayload]
+                  withCompletionHandler: completion];
+      [RNCallKeep endCallWithUUID:uuidString reason:1];
+    }
+  }];
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
